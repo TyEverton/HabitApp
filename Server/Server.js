07dddf1bd924b69e05b9a0ad9e105a6c1ea90d2c
Ty Everton <everton_915@yahoo.com>
2021-08-30 11:36:38 -0600

    changed file directory for line 10 in server.js

--- Server/Server.js
+++ Server/Server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, './habit-app/Components/Home')))
 app.use('/styles.css', express.static(path.join(__dirname, '/src/index.css')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 const PORT = process.env.PORT || 3000
